5d72a26a99d58fed6601745f2ac3ac0376c61d63
Kasun Peiris <kasun@karapincha.io>
2023-12-23 12:17:14 +0530

    testing hosted templates

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
-// This is a node script
+import chalk from "chalk";
 import inquirer from "inquirer";
-import { createSpinner } from "nanospinner";
-import fs from "fs";
-const readPackageJson = async () => {
-    const spinner = createSpinner("Loading package.json");
-    spinner.start();
-    // Read package.json from the current directory with fs
-    const packageJson = await fs.promises.readFile("package.json", "utf-8");
-    spinner.stop();
-    return packageJson;
-};
+import { setupPrettier, } from "./utils.js";
 inquirer
     .prompt([
 /* Pass your questions in here */
 ])
     .then(async (answers) => {
-    const packageJson = await readPackageJson();
-    console.log(packageJson);
+    // await installDevDependencies([
+    // 	"tailwindcss",
+    // 	"postcss",
+    // 	"autoprefixer",
+    // 	"prettier",
+    // 	"prettier-plugin-prisma",
+    // 	"prettier-plugin-tailwindcss",
+    // 	"prisma",
+    // 	"ts-node",
+    // 	"@ianvs/prettier-plugin-sort-imports",
+    // ]);
+    const rc = await setupPrettier();
+    console.log(rc);
+    console.log(chalk.green("Successfully setup prettier!"));
 })
     .catch((error) => {
     if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
     }
     else {
-        // Something else went wrong
     }
 });
---
+++ utils.ts
@@ -0,0 +1,46 @@
+import { createSpinner } from "nanospinner";
+import fs from "fs";
+import { execa } from "execa";
+
+export const readPackageJson = async () => {
+	const spinner = createSpinner("Loading package.json");
+	spinner.start();
+	const packageJson = await fs.promises.readFile("package.json", "utf-8");
+	const parsedPackageJson = JSON.parse(packageJson);
+	spinner.stop();
+	return parsedPackageJson;
+};
+
+export const installDependencies = async (dependencies: string[]) => {
+	const spinner = createSpinner("Installing dependencies");
+	spinner.start();
+	await execa("pnpm", ["install", "--save-dev", ...dependencies]);
+	spinner.stop();
+};
+
+export const installDevDependencies = async (dependencies: string[]) => {
+	const spinner = createSpinner("Installing dev dependencies");
+	spinner.start();
+	await execa("pnpm", ["install", "--save-dev", ...dependencies]);
+	spinner.stop();
+};
+
+export const setupPrettier = async () => {
+	const spinner = createSpinner("Setting up prettier");
+	spinner.start();
+	const prettierRc = await fs.promises.readFile(
+		"./templates/.prettierrc",
+		"utf-8"
+	);
+	const prettierIgnore = await fs.promises.readFile(
+		"./templates/.prettierignore",
+		"utf-8"
+	);
+
+	// add .prettierrc and prettierIgnore to root
+	await fs.promises.writeFile("./.prettierrc", prettierRc);
+	await fs.promises.writeFile("./.prettierignore", prettierIgnore);
+
+	spinner.stop();
+	return prettierRc;
+};
